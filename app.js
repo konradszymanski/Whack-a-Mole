3c9b509cc0f909311cffda9c1b4676335fd93d10
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-04-21 19:53:54 +0100

    updated

--- app.js
+++ app.js
@@ -5,8 +5,38 @@
 //random numbers to change a class of the one elelment of grid layout
 //
 //
+//const start = document.querySelector('#startBtn');
+//const start2 = document.getElementById("startBtn")
+
+// function gridClick(id) {
+//     alert(id)
+// }
+
+// function asd() {
+//     alert('asdad')
+// }
 
 
-function gridClick(id) {
-    alert(id)
+
+
+// take all the id's
+// loop from 1 to 16 to kreate an array
+// setInterval to random nums
+
+let allId = []
+
+//let elements = document.querySelectorAll(allId)
+
+function startGame() {
+
+    for (let i = 1; i <= 16; i++) {
+        allId.push(i);
+    }
+    console.log(allId)
+
+
+    // setInterval()
+
+    // id.style.backgroundColor = 'gray'
+
 }
