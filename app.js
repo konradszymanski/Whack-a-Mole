6fc0ba0f8c133794f7c8f6f0d3d1bed8c7aaaefe
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-04-21 20:53:21 +0100

    random and move

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
-//alert('asdasd')
-//grid same class
-//create grid board 
-//create .mole class
-//random numbers to change a class of the one elelment of grid layout
-//
-//
-//const start = document.querySelector('#startBtn');
-//const start2 = document.getElementById("startBtn")
-
-// function gridClick(id) {
-//     alert(id)
-// }
-
-// function asd() {
-//     alert('asdad')
-// }
+const items = document.querySelectorAll('.grid-item');
+const startButton = document.getElementById('startBtn');
 
 
+function gridClick(id) {
+    alert(id)
+}
 
-
-// take all the id's
-// loop from 1 to 16 to kreate an array
-// setInterval to random nums
-
-let allId = []
-
-//let elements = document.querySelectorAll(allId)
-
-function startGame() {
-
-    for (let i = 1; i <= 16; i++) {
-        allId.push(i);
-    }
-    console.log(allId)
-
-
-    // setInterval()
-
-    // id.style.backgroundColor = 'gray'
+const randomItems = () => {
+    items.forEach(item => {
+        item.classList.remove('moleAlive')
+    })
+    let randomItems = items[Math.floor(Math.random() * 16)]
+    randomItems.classList.add('moleAlive')
 
 }
+
+const move = () => {
+    setInterval(randomItems, 100)
+}
+
+startButton.addEventListener('onclick', move())
